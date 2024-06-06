f548ccd0db7aee906f48f1a4d36912e25be2bb26
shashankpantiitbhilai <shashankpant94115@gmail.com>
2024-06-06 17:50:24 +0530

    handling cross orign request

--- Backend/index.js
+++ Backend/index.js
@@ -1,31 +1,32 @@
 const express = require("express");
-// eslint-disable-next-line node/no-unpublished-require
 const cors = require("cors");
 const routes_auth = require("./routes/auth");
 const routes_general = require("./routes/route");
-const routes_admin = require("./routes/admin")
+const routes_admin = require("./routes/admin");
 const routes_classes = require("./routes/classes");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const { connectDB } = require("./db");
 const myPassport = require("./models/passportConfig"); // Adjust the path accordingly
-const MemoryStore = require('memorystore')(session)
+const MemoryStore = require('memorystore')(session);
 require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-  origin: 'https://edu-gainer-s.vercel.app/', // Replace with your frontend's URL
+  origin: 'https://edu-gainer-s-frontend.vercel.app', // Replace with your frontend's URL
   credentials: true, // Enable credentials (e.g., cookies)
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Add all methods you will use
+  allowedHeaders: ['Content-Type', 'Authorization'], // Add all headers you will use
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Enable pre-flight requests for all routes
 
 // Connect to MongoDB
 connectDB();
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
 
 app.use(session({
   secret: 'keyboard cat',
@@ -36,12 +37,11 @@ app.use(session({
   })
 }));
 
-
 app.use(myPassport.initialize());
 app.use(myPassport.session());
 
 app.get("/", (_req, res) => {
-  res.redirect("https://edu-gainer-s.vercel.app/");
+  res.redirect("https://edu-gainer-s-frontend.vercel.app");
 });
 
 // Mount your route handlers
@@ -49,6 +49,7 @@ app.use("/", routes_general);
 app.use("/auth", routes_auth);
 app.use("/admin", routes_admin);
 app.use("/classes", routes_classes);
+
 // Start the server
 app.listen(process.env.PORT || 8000, () => {
   console.log(`connected to port ${process.env.PORT || 8000}`);
