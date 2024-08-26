7f5bd3d8c0201241779d8be3fc129e5b9614829f
shashankpantiitbhilai <shashankpant94115@gmail.com>
2024-08-26 19:59:33 +0530

    updating ui of feedback form

--- Frontend/src/Components/feedback/feedback.jsx
+++ Frontend/src/Components/feedback/feedback.jsx
@@ -2,15 +2,25 @@ import React from "react";
 
 const FeedbackForm = () => {
   return (
-    <div style={{ textAlign: "center", margin: "20px" }}>
-      <h2>We Value Your Feedback</h2>
+    <div
+      style={{ textAlign: "center", margin: "20px auto", maxWidth: "800px" }}
+    >
+      <h2>Share Your Thoughts with EduGainers</h2>
+      <p>
+        We appreciate your input! Help us improve your experience by providing
+        feedback on our website and our facilities.
+      </p>
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLScFfbN_g08SZJ-obkAv8Ib2-7YP5wcfMgHGir1qkonynkMCuA/viewform?embedded=true"
-        width="640"
-        height="967"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        width="100%"
+        height="800"
+      
+        style={{
+          border: "none",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        }}
+        title="EduGainers Feedback Form"
       >
         Loading…
       </iframe>
