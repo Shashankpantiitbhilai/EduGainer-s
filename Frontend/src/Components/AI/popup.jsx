59911f6bf8507954c036c3035c0d253a3194276a
shashankpantiitbh <shashankp@iitbhilai.ac.in>
2025-07-05 13:41:13 +0530

    update ui

--- Frontend/src/Components/AI/popup.jsx
+++ Frontend/src/Components/AI/popup.jsx
@@ -31,7 +31,7 @@ import {
   Login as LoginIcon,
   PersonAdd as SignUpIcon,
 } from "@mui/icons-material";
-import { lightTheme } from "../";
+import { lightTheme } from "../../theme";
 import robotIcon from "../../images/AI-chatbot.jpg";
 import { AdminContext } from "../../App";
 import SoundControl from './soundMode';
